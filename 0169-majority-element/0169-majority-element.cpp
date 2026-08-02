b1f8fbca7f8acea5809c0fddca7e046c3dd924b1
Shubhi Srivastava <srivastavashubhi718@gmail.com>
2026-08-02 15:52:14 +0530

    Time: 1438 ms (6.71%), Space: 28.2 MB (39.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,7 +6,7 @@ public:
        
         for(int i = 0 ; i < n ;i++){
                int cnt = 0; 
-            for(int j = 0; j< n; j++){
+            for(int j = i; j< n; j++){
                 if(nums[i]==nums[j]){
                  cnt++;
                 }
